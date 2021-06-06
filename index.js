df78c350c50a577bdb8cedd32de9bf8e59d3ae80
KrishtiGupta <85436578+KrishtiGupta@users.noreply.github.com>
2021-06-06 15:54:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./cpmponents/App";
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
